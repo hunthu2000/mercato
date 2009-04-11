3450c76e1b876bfb180e82050dee2518c1bcea92
huage.chen <huage.chen@5ba9deda-2532-11de-a101-5ba453ef2bb8>
2009-04-11 23:44:45 +0000

    log4j integration

--- trunk/mercato/src/main/java/com/alten/mercato/server/controller/DemoServiceImpl.java
+++ trunk/mercato/src/main/java/com/alten/mercato/server/controller/DemoServiceImpl.java
@@ -1,10 +1,11 @@
 package com.alten.mercato.server.controller;
 
+
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.gwtwidgets.server.spring.ServletUtils;
 
 import com.alten.mercato.client.service.DemoService;
@@ -15,14 +16,12 @@ import com.alten.mercato.client.service.DemoService;
 
 @Service("demoController")
 public class DemoServiceImpl  implements DemoService  {
-
-
 	
 	// log4j
-	private Log log  = LogFactory.getLog(getClass());
+	Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
 	
 	public String getString() {
-		
+		logger.info("Getting string in the controller" );
 		
 		return "Mercato GWT user: " + ServletUtils.getRequest().getUserPrincipal().getName();
 	}
@@ -30,14 +29,14 @@ public class DemoServiceImpl  implements DemoService  {
 	public boolean logout() throws Exception{
 		try {
 			HttpSession session = ServletUtils.getRequest().getSession(false);
-			log.debug("Logout request user : session id " + session.getId() + " user " + session.getAttribute("User") );
+			logger.info("Logout request user : session id " + session.getId() + " user " + session.getAttribute("User") );
 			
 			ServletUtils.getRequest().getSession(false).removeAttribute("User");
 			ServletUtils.getRequest().getSession(false).invalidate();
 			
 			return true;
 		} catch (Exception e) {
-			log.error(e.getMessage(), e);	
+			logger.error(e.getMessage(), e);	
 			throw e;
 		}
 	}
