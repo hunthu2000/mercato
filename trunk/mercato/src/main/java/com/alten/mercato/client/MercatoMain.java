ad0e060fe087314ec7ad9b713e2025c14a8b6bc3
huage.chen <huage.chen@5ba9deda-2532-11de-a101-5ba453ef2bb8>
2009-04-21 12:18:12 +0000

    drag & drop between two trees

--- trunk/mercato/src/main/java/com/alten/mercato/client/MercatoMain.java
+++ trunk/mercato/src/main/java/com/alten/mercato/client/MercatoMain.java
@@ -9,6 +9,7 @@ import com.alten.mercato.client.data.ds.PersonDataSource;
 import com.alten.mercato.client.service.DemoService;
 import com.alten.mercato.client.service.PersonService;
 import com.alten.mercato.client.ui.framework.widget.CustomWaitDialog;
+import com.alten.mercato.client.ui.util.ConstantsMercato;
 import com.alten.mercato.server.model.Departement;
 import com.alten.mercato.server.model.Personne;
 import com.google.gwt.core.client.EntryPoint;
@@ -19,10 +20,22 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.smartgwt.client.data.DSCallback;
 import com.smartgwt.client.data.DSRequest;
 import com.smartgwt.client.data.DSResponse;
+import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.DragDataAction;
+import com.smartgwt.client.types.TreeModelType;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.HTMLPane;
+import com.smartgwt.client.widgets.Img;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.events.DropEvent;
+import com.smartgwt.client.widgets.events.DropHandler;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
 import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.HStack;
+import com.smartgwt.client.widgets.layout.VStack;
+import com.smartgwt.client.widgets.tree.Tree;
 import com.smartgwt.client.widgets.tree.TreeGrid;
 import com.smartgwt.client.widgets.tree.TreeNode;
 
@@ -39,11 +52,14 @@ public class MercatoMain implements EntryPoint {
 	private PersonDataSource dsMyDepartmentConsultants = new PersonDataSource("myDepartment");
 	private PersonDataSource dsOtherDepartmentConsultants = new PersonDataSource("otherDepartments");
 
+	/* (non-Javadoc)
+	 * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+	 */
 	public void onModuleLoad() {
 		
 		HLayout hlayout = new HLayout();  
         hlayout.setWidth(800);  
-        hlayout.setHeight100();  
+        hlayout.setHeight(600);  
         hlayout.setMembersMargin(5);  
 		hlayout.setPadding(5);
 		
@@ -68,7 +84,6 @@ public class MercatoMain implements EntryPoint {
 		Canvas canvasLogout = new Canvas();
 		canvasLogout.addChild(paneLinkLogout);
 		
-
 		final Label quoteText = new Label();
 		
 	
@@ -112,8 +127,7 @@ public class MercatoMain implements EntryPoint {
 					return;
 				}
 				System.out.println(result.size() + " consultants for this dd found");
-				createTreeNodes(result);
-				openAllNodesOfListGrid(tgMyDepartmentConsultants);
+				createMyConsultantTreeNodes(result);
 				dlg.hide();
 			}
 
@@ -127,6 +141,9 @@ public class MercatoMain implements EntryPoint {
 		dlg.show();
 	}
 	
+	/**
+	 * 
+	 */
 	private void loadOtherDepartmentListConsultantsByRPC() {
 
 		final CustomWaitDialog dlg = CustomWaitDialog.getInstance();
@@ -139,7 +156,7 @@ public class MercatoMain implements EntryPoint {
 					return;
 				}
 				System.out.println(result.size() + " consultants for this dd found");
-				createOtherDepartmentTreeNodes(result);
+				createOtherDepartmentConsultantTreeNodes(result);
 				dlg.hide();
 			}
 
@@ -153,6 +170,9 @@ public class MercatoMain implements EntryPoint {
 		dlg.show();
 	}
 	
+	/**
+	 * 
+	 */
 	private void loadOtherDepartmentsByRPC() {
 
 		final CustomWaitDialog dlg = CustomWaitDialog.getInstance();
@@ -165,10 +185,8 @@ public class MercatoMain implements EntryPoint {
 					return;
 				}
 				System.out.println(result.size() + " department for this dd found");
-				for (Departement dpmt: result) {
-					dsOtherDepartmentConsultants.addData(createDsDpmtNode( String.valueOf(dpmt.getDepId()), dpmt.getDepLib(),"silk/chart_organisation.png", "root", dpmt));
-				}
-				openAllNodesOfListGrid(tgOtherDepartmentConsultants);
+				
+				createOtherDepartmentTreeNodes(result);
 				dlg.hide();
 			}
 
@@ -187,74 +205,174 @@ public class MercatoMain implements EntryPoint {
 	 * 
 	 */
 	private void initMyDepartmentTreeGrid() {
-		tgMyDepartmentConsultants.setAutoFetchData(true);
-		tgMyDepartmentConsultants.setLoadDataOnDemand(false); 
-		tgMyDepartmentConsultants.setHeight(400);
+		tgMyDepartmentConsultants.setHeight100();
 		tgMyDepartmentConsultants.setWidth("50%");
 		tgMyDepartmentConsultants.setCanDragRecordsOut(true);
-		tgMyDepartmentConsultants.setDragDataAction(DragDataAction.COPY);
+		tgMyDepartmentConsultants.setDragDataAction(DragDataAction.MOVE);
 		tgMyDepartmentConsultants.setCanAcceptDroppedRecords(true);
-		tgMyDepartmentConsultants.setDataSource(dsMyDepartmentConsultants);
+		tgMyDepartmentConsultants.setAlternateRecordStyles(true);
+		tgMyDepartmentConsultants.setShowHeader(true);
+		tgMyDepartmentConsultants.setLeaveScrollbarGap(false);
 		
+		Tree tree = new Tree();
+		tree.setModelType(TreeModelType.PARENT);
+		TreeNode rootNode = createRootNode("root", "My department", ConstantsMercato.ICON_DPMT, null);
+		tree.setRoot(rootNode);
+		rootNodeMyDepartmentConsultants = createRootNode("rootMyDpmt", "My department", ConstantsMercato.ICON_DPMT, null);
+		tree.add(rootNodeMyDepartmentConsultants, rootNode);
+		//rootNodeMyDepartmentConsultants.setCanAcceptDrop(true);
+		tgMyDepartmentConsultants.setData(tree);
+		tree.setNameProperty(PersonDataSource.KEY_LABEL);
+		tgMyDepartmentConsultants.getData().openAll();
 		
-		rootNodeMyDepartmentConsultants = createDsNode("root", "My department", "silk/chart_organisation.png", "", null);
-		dsMyDepartmentConsultants.addData(rootNodeMyDepartmentConsultants);
 		
 	}
 	
+	/**
+	 * 
+	 */
 	private void initOtherDepartmentTreeGrid() {
-		tgOtherDepartmentConsultants.setAutoFetchData(true);
-		tgOtherDepartmentConsultants.setLoadDataOnDemand(false); 
-		tgOtherDepartmentConsultants.setHeight(400);
+		tgOtherDepartmentConsultants.setHeight100();
 		tgOtherDepartmentConsultants.setWidth("50%");
 		tgOtherDepartmentConsultants.setCanDragRecordsOut(true);
-		tgOtherDepartmentConsultants.setDragDataAction(DragDataAction.COPY);
+		tgOtherDepartmentConsultants.setDragDataAction(DragDataAction.MOVE);
 		tgOtherDepartmentConsultants.setCanAcceptDroppedRecords(true);
-		tgOtherDepartmentConsultants.setCanReorderRecords(false);
-		tgOtherDepartmentConsultants.setDataSource(dsOtherDepartmentConsultants);
+		tgOtherDepartmentConsultants.setAlternateRecordStyles(true);
+		tgOtherDepartmentConsultants.setShowHeader(true);
+		tgOtherDepartmentConsultants.setLeaveScrollbarGap(false);
+		
+		tgOtherDepartmentConsultants.setHeight100();
+		tgOtherDepartmentConsultants.setWidth("50%");
+		tgMyDepartmentConsultants.setCanDragRecordsOut(true);
+		tgMyDepartmentConsultants.setDragDataAction(DragDataAction.MOVE);
+		tgMyDepartmentConsultants.setCanAcceptDroppedRecords(true);
+		tgMyDepartmentConsultants.setAlternateRecordStyles(true);
+		tgMyDepartmentConsultants.setShowHeader(true);
+		tgMyDepartmentConsultants.setLeaveScrollbarGap(false);
+		
+		Tree tree = new Tree();
+		tree.setModelType(TreeModelType.PARENT);
+		TreeNode rootNode = createRootNode("root", "Other departments", ConstantsMercato.ICON_DPMT, null);
+		tree.setRoot(rootNode);
+		rootNodeOtherDepartmentConsultants = createRootNode("rootOtherDpmt", "Other departments", ConstantsMercato.ICON_DPMT, null);
+		//rootNodeOtherDepartmentConsultants.setCanAcceptDrop(true);
+		tree.add(rootNodeOtherDepartmentConsultants, rootNode);
+		tree.setNameProperty(PersonDataSource.KEY_LABEL);
+		tgOtherDepartmentConsultants.setData(tree);
+		tgOtherDepartmentConsultants.getData().openAll();
 		
 		
-		rootNodeOtherDepartmentConsultants = createDsNode("root", "Other departments", "silk/chart_organisation.png", "", null);
-		dsOtherDepartmentConsultants.addData(rootNodeOtherDepartmentConsultants);
-		
+//		tgOtherDepartmentConsultants.addDropHandler(new DropHandler() {
+//
+//			public void onDrop(DropEvent event) {
+//				ListGridRecord[] records = tgOtherDepartmentConsultants.getSelection();
+//				
+//			}
+//			
+//		});
 	}
 	
-	private TreeNode createDsNode(String id, String label, String icon, String parent, Personne obj) {
+	private TreeNode createRootNode(String id, String label, String icon, Object obj) {
 		TreeNode node = new TreeNode();
 		node.setAttribute(PersonDataSource.KEY_ID, id);
 		node.setAttribute(PersonDataSource.KEY_LABEL, label);
 		node.setAttribute(PersonDataSource.KEY_ICON, icon);
-		node.setAttribute(PersonDataSource.KEY_PARENT, parent);
 		node.setAttribute(PersonDataSource.KEY_OBJECT, obj);
-		node.setIsFolder(false);
 		return node;
 	}
+
 	
-	private TreeNode createDsDpmtNode(String id, String label, String icon, String parent, Departement obj) {
-		TreeNode node = new TreeNode();
-		node.setAttribute(PersonDataSource.KEY_ID, id);
-		node.setAttribute(PersonDataSource.KEY_LABEL, label);
-		node.setAttribute(PersonDataSource.KEY_ICON, icon);
-		node.setAttribute(PersonDataSource.KEY_PARENT, parent);
-		node.setAttribute(PersonDataSource.KEY_OBJECT, obj);
-		node.setIsFolder(true);
-		return node;
-	}
-	
-	private void createTreeNodes(List<Personne> persons) {
-		for (Personne person: persons) {
-			dsMyDepartmentConsultants.addData(createDsNode( String.valueOf(person.getPerId()), person.getPerPrenom() + " "+ person.getPerNom(),"icons/16/person.png", "root", person));
+	/**
+	 * @param persons
+	 */
+	private void createOtherDepartmentTreeNodes(List<Departement> departements) {
+	/*	for (Personne person: persons) {
+			dsMyDepartmentConsultants.addData(createDsNode( String.valueOf(person.getPerId()), person.getPerPrenom() + " "+ person.getPerNom(),ConstantsMercato.ICON_USER, "root", person));
 		
-		}
-	}
-	
-	private void createOtherDepartmentTreeNodes(List<Personne> persons) {
-		for (Personne person: persons) {
-			dsOtherDepartmentConsultants.addData(createDsNode( String.valueOf(person.getPerId()), person.getPerPrenom() + " "+ person.getPerNom(),"icons/16/person.png", String.valueOf(person.getDepartement().getDepId()), person));
+		}*/
 		
+		Tree tree = tgOtherDepartmentConsultants.getTree();
+		Object obj = null;
+		for (Departement departement: departements) {
+			TreeNode node = new TreeNode();
+			node.setAttribute(PersonDataSource.KEY_ID, String.valueOf(departement.getDepId()));
+			node.setAttribute(PersonDataSource.KEY_LABEL, departement.getDepLib());
+			node.setAttribute(PersonDataSource.KEY_ICON, ConstantsMercato.ICON_DPMT);
+			node.setAttribute(PersonDataSource.KEY_OBJECT, obj);
+			//node.setAttribute(PersonDataSource.KEY_OBJECT, null);
+			//node.setAttribute(PersonDataSource.KEY_OBJECT, null);
+			//node.setChildren(null);
+			//node.setCanAcceptDrop(true);
+			node.setIsFolder(true);
+			TreeNode parentNode = tree.findById("rootOtherDpmt");
+			tree.add(node, parentNode);
 		}
+		
+		tgOtherDepartmentConsultants.setData(tree);
+		tgOtherDepartmentConsultants.getData().openAll();
 	}
 	
+	
+	/**
+	 * @param persons
+	 */
+	private void createMyConsultantTreeNodes(List<Personne> persons) {
+		/*for (Personne person: persons) {
+			dsOtherDepartmentConsultants.addData(createOtherDepartementDsNode( String.valueOf(person.getPerId()), person.getPerPrenom() + " "+ person.getPerNom(),ConstantsMercato.ICON_USER, String.valueOf(person.getDepartement().getDepId()), person));
+		
+		}*/
+		
+		Tree tree = tgMyDepartmentConsultants.getTree();
+		
+		for (Personne person: persons) {
+			TreeNode node = new TreeNode();
+			node.setAttribute(PersonDataSource.KEY_ID, String.valueOf(person.getPerId()));
+			node.setAttribute(PersonDataSource.KEY_LABEL, person.getPerPrenom() + " "+ person.getPerNom());
+			node.setAttribute(PersonDataSource.KEY_ICON, ConstantsMercato.ICON_USER);
+			TreeNode parentNode = tree.findById("rootMyDpmt");
+			node.setAttribute(PersonDataSource.KEY_OBJECT, person);
+			//node.setCanAcceptDrop(false);
+			
+			tree.add(node, parentNode);
+		}
+		
+		tgMyDepartmentConsultants.setData(tree);
+		tgMyDepartmentConsultants.getData().openAll();
+	}
+	
+	
+	/**
+	 * @param persons
+	 */
+	private void createOtherDepartmentConsultantTreeNodes(List<Personne> persons) {
+		/*for (Personne person: persons) {
+			dsOtherDepartmentConsultants.addData(createOtherDepartementDsNode( String.valueOf(person.getPerId()), person.getPerPrenom() + " "+ person.getPerNom(),ConstantsMercato.ICON_USER, String.valueOf(person.getDepartement().getDepId()), person));
+		
+		}*/
+		
+		Tree tree = tgOtherDepartmentConsultants.getTree();
+		
+		for (Personne person: persons) {
+			TreeNode node = new TreeNode();
+			node.setAttribute(PersonDataSource.KEY_ID, String.valueOf(person.getPerId()));
+			node.setAttribute(PersonDataSource.KEY_LABEL, person.getPerPrenom() + " "+ person.getPerNom());
+			node.setAttribute(PersonDataSource.KEY_ICON, ConstantsMercato.ICON_USER);
+			
+			TreeNode parentNode = tree.findById(String.valueOf(person.getDepartement().getDepId()));
+			System.out.println("parent id :" + parentNode.getAttribute(ConstantsMercato.KEY_ID));
+			node.setAttribute(PersonDataSource.KEY_OBJECT, person);
+			//node.setCanAcceptDrop(true);
+			
+			tree.add(node, parentNode);
+		}
+		
+		tgOtherDepartmentConsultants.setData(tree);
+		tgOtherDepartmentConsultants.getData().openAll();
+	}
+	
+	/**
+	 * @param treegrid
+	 */
 	public void openAllNodesOfListGrid(final TreeGrid treegrid) {
 		Timer timer = new Timer() {
 
@@ -288,6 +406,5 @@ public class MercatoMain implements EntryPoint {
 		};
 		timer.scheduleRepeating(1000);
 	}
-
 
 }
